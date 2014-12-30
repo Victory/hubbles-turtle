f56c09f04e9a1ee862b838e96dea5810944bb22f
Victory <git@dfhu.org>
2014-12-29 19:37:47 -0500

    still quite wrong in a lot of ways, but getting closer. Shows both photons that are always in  hubble sphere, those that cross and those that never will

--- turtles-all-the-way-down.py
+++ turtles-all-the-way-down.py
@@ -5,30 +5,41 @@ from random import random
 from time import sleep
 from math import pi, sin, cos, sqrt
 
-# TODO: make it so the hubble constant is around 70
+dt = 1.0 # time step
+age = 3.0 # age of the universe
+c = 20.0 # speed of light
+# hubble length
+hubble_length = c*age
+# hubble constant
+h0 = 1/c # hubble constant
+# cosmic scale factor
+cosmic_scale_factor = 1.1
+cosmic_scale_step = .01
+print(age)
 
-h0 = 1 # hubble constant
-c = 5 # speed of light
 
 stars = list()
 angles = list()
 photons = list()
-steps = 2
+steps = 4
 step = 2*pi / steps
 hubbleturtle = Turtle(shape="turtle")
 hubbleturtle.penup()
-hubbleturtle.setpos((0, -100))
+
 
 for ii in range(steps):
+
+
+
     angle = ii * step
     angles.append(angle)
-    x = cos(angle) * random() * 200
-    y = sin(angle) * random() * 200
+    x = cos(angle) * random() * 5 * hubble_length
+    y = sin(angle) * random() * 5 * hubble_length
 
     stars.append(Turtle(shape="triangle"))
 
     r = sqrt(x*x + y*y)
-    if r < 100:
+    if r < hubble_length:
         color = "blue"
     else:
         color = "red"
@@ -41,7 +52,7 @@ for ii in range(steps):
     stars[ii].setheading(angle)
 
     photons.append(Turtle(shape="circle"))
-    photons[ii].penup()
+    #photons[ii].penup()
     photons[ii].speed(10)
     photons[ii].color(color)
     photons[ii].setpos((x, y))
@@ -50,39 +61,42 @@ for ii in range(steps):
 
 
 screen = getscreen()
-# should be like c/h0
-inner_universe = 100
-# cosmic scale factor
-expanshion_rate = 1
-# hubble flow
-expanshion_step = .01
+
+hubbleturtle.setpos((0, -hubble_length))
 
 for i in range(200):
     #screen.tracer(1000)
-     
+
+    distance_light_travels_per_step = c * dt * cosmic_scale_factor
+
+    print(cosmic_scale_factor)
+
     for ii, t in enumerate(stars):
         pos = t.pos()
         r = sqrt(pos[0]*pos[0] + pos[1]*pos[1])
-        newr = r * expanshion_rate
+        newr = r * cosmic_scale_factor
         dr = newr - r
         t.forward(dr)
 
         # TODO: stop photons near origin
         pos = photons[ii].pos()
         r = sqrt(pos[0]*pos[0] + pos[1]*pos[1])
-        newr = r * expanshion_rate
+        newr = r * cosmic_scale_factor
         dr = newr - r
-        print(ii, dr)
+        print(ii, dr, distance_light_travels_per_step)
         photons[ii].backward(dr)
-        photons[ii].forward(c)
+        photons[ii].forward(distance_light_travels_per_step)
+
+    # accelerting universe
+    cosmic_scale_factor += cosmic_scale_step
+
 
     hubbleturtle.pendown()
-    hubbleturtle.circle(inner_universe)
+    hubbleturtle.circle(hubble_length)
     hubbleturtle.penup()
-    inner_universe *= expanshion_rate
-    expanshion_rate += expanshion_step
+    hubble_length += distance_light_travels_per_step
     
-    hubbleturtle.setpos((0, -inner_universe))
+    hubbleturtle.setpos((0, -hubble_length))
 
     screen.update()
     sleep(.01)
