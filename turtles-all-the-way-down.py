440b65e1a2560a8bf5ac63be763e2f24276f6b87
Victory <git@dfhu.org>
2015-01-04 16:50:25 -0500

    fix headings of stars/photons

--- turtles-all-the-way-down.py
+++ turtles-all-the-way-down.py
@@ -16,24 +16,23 @@ hubble_length = c/h
 # cosmic scale factor
 cosmic_scale_factor = 1.1
 cosmic_scale_step = .01
-print(age)
-
 
 stars = list()
 angles = list()
 photons = list()
-steps = 4
+steps = 7
 step = 2*pi / steps
 hubbleturtle = Turtle(shape="turtle")
 hubbleturtle.penup()
 
 
-rand_scaling = 2
+rand_scaling = 5
 for ii in range(steps):
     angle = ii * step
     angles.append(angle)
-    x = cos(angle) * random() * rand_scaling * hubble_length
-    y = sin(angle) * random() * rand_scaling * hubble_length
+    rnd = random()
+    x = cos(angle) * rnd * rand_scaling * hubble_length
+    y = sin(angle) * rnd * rand_scaling * hubble_length
 
     stars.append(Turtle(shape="triangle"))
 
@@ -45,7 +44,7 @@ for ii in range(steps):
         
     stars[ii].color(color)
     stars[ii].speed(10)
-    stars[ii].penup()
+    #stars[ii].penup()
     stars[ii].setpos((x, y))
     stars[ii].radians()
     stars[ii].setheading(angle)
@@ -63,19 +62,17 @@ screen = getscreen()
 
 hubbleturtle.setpos((0, -hubble_length))
 
-for i in range(200):
+for i in range(30):
     #screen.tracer(1000)
 
     distance_light_travels_per_step = c * dt # should there be a correct for change of dr in time dt? Does that cause the increase of vrec?
-
-    print(distance_light_travels_per_step)
 
     for ii, t in enumerate(stars):
         # move stars
         pos = t.pos()
         r = sqrt(pos[0]*pos[0] + pos[1]*pos[1])
         dr = h * r * dt
-        print(dr)
+        #print(dr)
         t.forward(dr)
 
         # move planet
@@ -87,13 +84,8 @@ for i in range(200):
 
         r = sqrt(pos[0]*pos[0] + pos[1]*pos[1])
         dr = h * r * dt
-        print(distance_light_travels_per_step, dr, distance_light_travels_per_step - dr)
+        #print(distance_light_travels_per_step, dr, distance_light_travels_per_step - dr)
         photons[ii].forward(distance_light_travels_per_step - dr)
-
-
-    # "velocity" of the hubble spher's recession
-    cosmic_scale_factor += cosmic_scale_step
-
 
     hubbleturtle.pendown()
     hubbleturtle.circle(hubble_length)
@@ -101,7 +93,7 @@ for i in range(200):
 
     hubble_length = c/h
     vrec = h * hubble_length
-    print("vrec", vrec)
+    #print("vrec", vrec)
     hubbleturtle.setpos((0, -hubble_length))
 
     age += dt
